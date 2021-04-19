f44a46c28b04d3aa616f6d6dde5edf13b0987d36
Gustavo Garzaki <garzaki@pm.me>
2021-04-19 10:04:30 -0500

    format

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-import React, { render } from "react-dom";
+import React, { render } from "react-dom";
 import { App } from "src/app";
 
-const injectionNode = document.getElementById("app")
+const injectionNode = document.getElementById("app");
 
-render(<App />, injectionNode)
+render(<App />, injectionNode);
